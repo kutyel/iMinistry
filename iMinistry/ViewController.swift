93fa7712e2095a59ec4eb61956bfc52516a595d0
kutyel <flaviocorpa@gmail.com>
2014-12-28 18:35:41 +0100

    Initial Commit

---
+++ iMinistry/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iMinistry
+//
+//  Created by Flavio Corpa on 28/12/14.
+//  Copyright (c) 2014 Flavio Corpa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
