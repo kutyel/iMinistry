8704adf85361f0756043b85e1e5129eafcaf5595
kutyel <flaviocorpa@gmail.com>
2015-04-08 00:42:55 +0200

    Implement iMinistryFooterView

--- iMinistry/AnualReportChartViewController.swift
+++ iMinistry/AnualReportChartViewController.swift
@@ -10,21 +10,19 @@ import UIKit
 
 class AnualReportChartViewController: UIViewController, JBBarChartViewDelegate, JBBarChartViewDataSource {
 
-    let staticData: [CGFloat] = [ 50, 60, 70, 45, 30, 10, 70, 80, 75, 35, 70, 25 ]
+    let staticData: [CGFloat] = [ 50, 60, 70, 45, 30, 25, 60, 65, 75, 35, 70, 25 ]
     
     // Constants
     
+    let anualReportNumBars: UInt = 12
     let anualReportChartHeight: CGFloat = 250.0
     let anualReportChartPadding: CGFloat = 10.0
-    let kanualReportChartHeaderHeight: CGFloat = 80.0
+    let anualReportChartHeaderHeight: CGFloat = 80.0
     let anualReportChartHeaderPadding: CGFloat = 20.0
     let anualReportChartFooterHeight: CGFloat = 25.0
     let anualReportChartFooterPadding: CGFloat = 5.0
     let anualReportBarPadding: CGFloat = 1.0
-    let anualReportNumBars: UInt = 12
-    let anualReportMaxBarHeight: UInt = 10
-    let anualReportMinBarHeight: UInt = 5
-    let monthSymbols = NSDateFormatter().monthSymbols
+    let monthSymbols = NSDateFormatter().shortMonthSymbols
     
     // View Creation
     
@@ -36,18 +34,28 @@ class AnualReportChartViewController: UIViewController, JBBarChartViewDelegate, 
         let anualReportsChart = JBBarChartView()
         anualReportsChart.dataSource = self
         anualReportsChart.delegate = self
+        anualReportsChart.headerPadding = anualReportChartHeaderPadding
         anualReportsChart.frame = CGRectMake(anualReportChartPadding, anualReportChartPadding, self.view.bounds.size.width - (anualReportChartPadding * 2), anualReportChartHeight)
         
-        // JBBarChartFooterView 
+        // JBChartHeaderView
         /*
-        let chartFooter = UIView(frame: CGRectMake(anualReportChartPadding, ceil(self.view.bounds.size.height * 0.5) - ceil(anualReportChartFooterHeight * 0.5), self.view.bounds.size.width - (anualReportChartPadding * 2), anualReportChartFooterHeight))
-        chartFooter.padding = anualReportChartFooterPadding;
-        chartFooter.leftLabel.text = monthSymbols.first().uppercaseString
-        chartFooter.leftLabel.textColor = UIColor.lightGrayColor()
-        chartFooter.rightLabel.text = monthSymbols.last().uppercaseString
-        chartFooter.rightLabel.textColor = UIColor.lightGrayColor()
-        anualReportsChart.footerView = chartFooter
+        JBChartHeaderView *headerView = [[JBChartHeaderView alloc] initWithFrame:CGRectMake(kJBBarChartViewControllerChartPadding, ceil(self.view.bounds.size.height * 0.5) - ceil(kJBBarChartViewControllerChartHeaderHeight * 0.5), self.view.bounds.size.width - (kJBBarChartViewControllerChartPadding * 2), kJBBarChartViewControllerChartHeaderHeight)];
+        headerView.titleLabel.text = [kJBStringLabelAverageMonthlyTemperature uppercaseString];
+        headerView.subtitleLabel.text = kJBStringLabel2012;
+        headerView.separatorColor = kJBColorBarChartHeaderSeparatorColor;
+        anualReportsChart.headerView = headerView;
         */
+        
+        // iMinistryFooterView
+        
+        var footer = iMinistryFooterView(frame: CGRectMake(anualReportChartPadding, ceil(self.view.bounds.size.height * 0.5) - ceil(anualReportChartFooterHeight * 0.5), self.view.bounds.size.width - (anualReportChartPadding * 2), anualReportChartFooterHeight))
+        footer.padding = anualReportChartFooterPadding;
+        footer.leftLabel.text = monthSymbols.first?.uppercaseString
+        footer.leftLabel.textColor = UIColor.lightGrayColor()
+        footer.rightLabel.text = monthSymbols.last?.uppercaseString
+        footer.rightLabel.textColor = UIColor.lightGrayColor()
+        anualReportsChart.footerView = footer
+        
         self.view.addSubview(anualReportsChart)
         anualReportsChart.reloadData()
     }
@@ -68,6 +76,15 @@ class AnualReportChartViewController: UIViewController, JBBarChartViewDelegate, 
     }
     
     func barChartView(barChartView: JBBarChartView!, colorForBarViewAtIndex index: UInt) -> UIColor! {
-        return UIColor.blueColor()
+        return colorFromHex(0x4a6da7) // JW.ORG
+    }
+    
+    func barPaddingForBarChartView(barChartView: JBBarChartView!) -> CGFloat {
+        return self.anualReportBarPadding
+    }
+    
+    // Returns UIColor from hexadecimal value
+    func colorFromHex(hex: Int) -> UIColor {
+        return UIColor(red: ((CGFloat)((hex & 0xFF0000) >> 16))/255.0, green: ((CGFloat)((hex & 0xFF00) >> 8))/255.0, blue: ((CGFloat)(hex & 0xFF))/255.0, alpha: 1.0)
     }
 }
---
+++ iMinistry/Views/iMinistryFooterView.swift
@@ -0,0 +1,71 @@
+//
+//  iMinistryFooterView.swift
+//  iMinistry
+//
+//  Created by Flavio Corpa on 07/04/15.
+//  Copyright (c) 2015 Flavio Corpa. All rights reserved.
+//
+
+import UIKit
+
+class iMinistryFooterView: UIView {
+    
+    var padding: CGFloat
+    var leftLabel: UILabel
+    var rightLabel: UILabel
+
+    convenience override init(){
+        self.init(frame: CGRectZero)
+    }
+    
+    override init(frame: CGRect) {
+        padding = 0.0
+        leftLabel = UILabel()
+        rightLabel = UILabel()
+        super.init(frame: frame)
+        
+        self.backgroundColor = footerViewDefaultBackgroundColor
+        
+        self.padding = footerPolygonViewDefaultPadding
+        
+        leftLabel = UILabel()
+        leftLabel.adjustsFontSizeToFitWidth = true
+        leftLabel.font = UIFont(name: "HelveticaNeue-Light", size: 12.0)
+        leftLabel.textAlignment = .Left
+        leftLabel.shadowColor = UIColor.lightGrayColor()
+        leftLabel.shadowOffset = CGSizeMake(0, 1)
+        leftLabel.backgroundColor = UIColor.clearColor()
+        self.addSubview(leftLabel)
+        
+        rightLabel = UILabel()
+        rightLabel.adjustsFontSizeToFitWidth = true
+        rightLabel.font = UIFont(name: "HelveticaNeue-Light", size: 12.0)
+        rightLabel.textAlignment = .Right
+        rightLabel.shadowColor = UIColor.lightGrayColor()
+        rightLabel.shadowOffset = CGSizeMake(0, 1)
+        rightLabel.backgroundColor = UIColor.clearColor()
+        self.addSubview(rightLabel)
+    }
+
+    required init(coder: NSCoder) {
+        padding = 0.0
+        leftLabel = UILabel()
+        rightLabel = UILabel()
+        super.init(coder: coder)
+    }
+    
+    let footerPolygonViewDefaultPadding: CGFloat = 0.4
+    let footerViewDefaultBackgroundColor: UIColor = UIColor.whiteColor()
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        let xOffset: CGFloat = self.padding
+        let yOffset: CGFloat = 0
+        let width: CGFloat = ceil(self.bounds.size.width * 0.5) - self.padding
+        
+        self.leftLabel.frame = CGRectMake(xOffset, yOffset, width, self.bounds.size.height)
+        self.rightLabel.frame = CGRectMake(CGRectGetMaxX(leftLabel.frame), yOffset, width, self.bounds.size.height)
+    }
+    
+}
